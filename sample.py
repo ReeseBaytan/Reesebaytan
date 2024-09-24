0c9fad02a957309db90eae49b6a4e30b358370d9
Reese Baytan <reesokyut@gmail.com>
2024-09-24 19:31:51 +0800

    customer eligibility

---
+++ sample.py
@@ -0,0 +1,26 @@
+def customer_eligibility():
+    salary = float(input("Enter your salary: "))
+    loan_amount = float(input("Enter loan amount: "))
+    
+    if salary >= 30000 and loan_amount <= salary * 10:
+        print("Eligable to loan!")
+        months = int(input("months "))
+
+        #calculation
+        loan_interest = loan_amount * 1.10
+        monthly_payment = loan_interest / months
+        
+        print(f"Your loan interest with 10%: {loan_interest:.2f}")
+        print(f"Your monthy payment is: {monthly_payment:.2f}")
+
+
+    else:
+         if salary < 30000:
+            print("Not eligable to loan!")
+         if loan_amount > salary *10 :
+            print("Requested loan is too high!")
+
+
+
+
+customer_eligibility()
